a243d1b543fd5bbc21e79ed7260795a0a7ee9b4b
kra3 <kra3@kra3.in>
2012-04-12 12:07:12 +0530

    some meta and placeholders for other entitirs

--- pyga/entities.py
+++ pyga/entities.py
@@ -1,5 +1,27 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Arun KR (kra3) <the1.arun@gmail.com>"
+__license__ = "Simplified BSD"
+
+class Campaign(object):
+    pass
+
+
+class CustomVariable(object):
+    pass
+
+
+class Event(object):
+    pass
+
+
+class Item(object):
+    pass
+
+
 class Page(object):
     '''
+    Contains all parameters needed for tracking a page
 
     Keyword Arguments:
     path -- Page request URI, e.g. "/path/page.html", will be mapped to "utmp" parameter
@@ -7,6 +29,7 @@ class Page(object):
     charset -- Charset encoding (e.g. "UTF-8"), will be mapped to "utmcs" parameter
     referrer -- Referer URL, e.g. "http://www.example.com/path/page.html",  will be mapped to "utmr" parameter
     load_time -- Page load time in milliseconds, will be encoded into "utme" parameter.
+
     '''
     REFERRER_INTERNAL = '0'
 
@@ -30,3 +53,23 @@ class Page(object):
                 raise Exception('Page load time must be specified in integer milliseconds.')
 
         object.__setattr__(self, name, value)
+
+
+class Session(object):
+    pass
+
+
+class SocialInteraction(object):
+    pass
+
+
+class Tracker(object):
+    pass
+
+
+class Transaction(object):
+    pass
+
+
+class Visitor(object):
+    pass
