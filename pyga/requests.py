ddef4f1804ab0e897b8bff7b91f1cb58b19e1409
kra3 <kra3@kra3.in>
2012-04-15 02:08:18 +0530

    added request classses. They are just markers now

--- pyga/requests.py
+++ pyga/requests.py
@@ -2,3 +2,30 @@
 
 __author__ = "Arun KR (kra3) <the1.arun@gmail.com>"
 __license__ = "Simplified BSD"
+
+class EventRequest(object):
+    pass
+
+
+class HttpRequest(object):
+    pass
+
+
+class ItemRequest(object):
+    pass
+
+
+class PageViewRequest(object):
+    pass
+
+
+class Request(object):
+    pass
+
+
+class SocialInteractionRequest(object):
+    pass
+
+
+class TransactionRequest(object):
+    pass
