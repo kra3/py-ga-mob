084f802f651da2190c026479d7b6c7640afb50f5
kra3 <kra3@kra3.in>
2012-04-15 13:11:33 +0530

    Added Q object to Queue requests & changes in Request* Hierarchy

--- pyga/requests.py
+++ pyga/requests.py
@@ -1,31 +1,82 @@
 # -*- coding: utf-8 -*-
 
+from base import Config
+
 __author__ = "Arun KR (kra3) <the1.arun@gmail.com>"
 __license__ = "Simplified BSD"
 
-class EventRequest(object):
+
+Class Q(object):
+    REQ_ARRAY = []
+
+    def add_wrapped_request(self, req_wrapper):
+        self.REQ_ARRAY.append(req_wrapper)
+
+
+class GIFRequest(object):
+    '''
+
+    Properties:
+    type -- Indicates the type of request, will be mapped to "utmt" parameter
+    config --
+    x_forwarded_for --
+    user_agent --
+
+    '''
+    def __init__(self, config):
+        self.type = None
+        self.config = None
+        self.x_forwarded_for = None
+        self.user_agent = None
+        self.__Q = Q()
+        if isinstance(config, Config):
+            self.config = config
+
+    def build_http_request(self):
+        params = self.build_parameters()
+
+    def build_parameters(self):
+        return {}
+
+    def __send(self):
+        pass
+
+    def fire(self):
+        '''
+        Simply delegates to send() if config option "queue_requests" is disabled
+        else enqueues the request into Q object: you should call pyga.shutdowon
+        as last statement, to actually send out all queued requests.
+        '''
+        if config.queue_requests:
+            # Queuing results. You should call pyga.shutdown as last statement to send out requests.
+            self.__Q.add_wrapped_request((lambda: self.__send()))
+        else:
+            self.__send()
+
+
+class Request(GIFRequest):
+    def __init__(self, config, tracker=None, visitor=None, session=None):
+        super(self, Request).__init__(config)
+        self.tracker = tracker
+        self.visitor = visitor
+        self.session = session
+
+
+class ItemRequest(Request):
     pass
 
 
-class HttpRequest(object):
+class PageViewRequest(Request):
     pass
 
 
-class ItemRequest(object):
+class EventRequest(Request):
     pass
 
 
-class PageViewRequest(object):
+class SocialInteractionRequest(Request):
     pass
 
 
-class Request(object):
-    pass
-
-
-class SocialInteractionRequest(object):
-    pass
-
-
-class TransactionRequest(object):
+class TransactionRequest(Request):
     pass
