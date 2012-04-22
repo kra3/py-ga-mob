8eab559fea91ac333dc3b6448d6b3dc7a43ddff0
kra3 <kra3@kra3.in>
2012-04-22 17:37:18 +0530

    path was to be included in conf.py not in Makefile

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,8 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.insert(0, os.path.abspath('.'))
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
