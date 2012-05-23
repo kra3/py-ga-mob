39fb4496a0ed8351edc7680342ce48277b6f2a5e
Nick Sitarz <nsitarz@gmail.com>
2012-05-23 13:50:08 -0700

    added paperlabs to version for buildout

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
-ver = '2.1'
+ver = '2.1.paperlabs'
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_desc = open(README).read() + '\n\n'
 
