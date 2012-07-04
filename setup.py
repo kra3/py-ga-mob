8a11e79e52717b1ea78397b5e48b060b9ff1abf5
kra3 <kra3@kra3.in>
2012-07-04 19:11:45 +0530

    new version 2.3 which handles certain serialization scenarios around Visitor.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
-ver = '2.2'
+ver = '2.3'
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_desc = open(README).read() + '\n\n'
 
@@ -12,7 +12,7 @@ setup(name='pyga',
       version=ver,
       author='Arun K.R.',
       author_email='the1.arun@gmail.com',
-      url='http://kra3.github.com/py-ga-mob',
+      url='http://kra3.github.com/py-ga-mob/',
       license='Simplified BSD',
       description='Server side implemenation of Google Analytics in Python.',
       long_description=long_desc,
