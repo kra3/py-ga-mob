6bc9b6c4fae1f661fe80d39a6aa672a879b17473
Kirill Klenov <horneds@gmail.com>
2012-09-03 16:45:38 +0400

    ver 2.4.2

--- pyga/requests.py
+++ pyga/requests.py
@@ -10,7 +10,7 @@ import urllib2
 
 __author__ = "Arun KR (kra3) <the1.arun@gmail.com>"
 __license__ = "Simplified BSD"
-__version__ = '2.5'
+__version__ = '2.4.2'
 
 logger = logging.getLogger(__name__)
 
