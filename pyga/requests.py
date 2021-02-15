3c5fef82b6951f4bb15dc0ee0e71c773505e26c2
Michiel van Baak <michiel@vanbaak.info>
2021-02-15 19:34:25 +0100

    Revert "Dont depend on six for a function that is in future.utils"
    This reverts commit fc723da57526e61ada6179c2cb850a4d46b896d0.

--- pyga/requests.py
+++ pyga/requests.py
@@ -5,7 +5,7 @@ import calendar
 from math import floor
 from pyga.entities import Campaign, CustomVariable, Event, Item, Page, Session, SocialInteraction, Transaction, Visitor
 import pyga.utils as utils
-from future.utils import itervalues
+from six import itervalues
 try:
     from urllib import urlencode
     from urllib2 import Request as urllib_request
